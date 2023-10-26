e5bb9d65392d764e212fd358bb646ff6dedf0406
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2023-10-26 07:48:19 +0100

    init project

---
+++ appwrite-hacktoberfest/appwrite-hacktoberfest/Shared/Constant/Database.swift
@@ -0,0 +1,17 @@
+//
+//  Database.swift
+//  appwrite-hacktoberfest
+//
+//  Created by Muhideen Mujeeb Adeoye on 02/10/2023.
+//
+
+import Foundation
+
+enum Database: String {
+    case habit = "[DATABASE_ID]"
+}
+
+
+enum DatabaseCollections : String {
+    case habits = "[COLLECTION_ID]"
+}
